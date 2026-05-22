278a3c57391beb675e0205e24eec9a82a944036c
amxnandxni <amxnandxni@gmail.com>
2026-05-22 19:15:13 +0530

    Add files via upload
    Day 26 of Web Development Journey:
    Started Learning Javascript

---
+++ Day26.js
@@ -0,0 +1,27 @@
+let name = "Dhoni";
+let result = name + 7;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(result);
+console.log(typeof name);
+console.log(typeof result);
+console.log(typeof p);
+console.log(typeof q);
+console.log(typeof r);
+const batter = {
+    name: "Virat",
+    No: 18
+};
+batter.No = 7;
+console.log(batter);
+batter.city = "Rcb";
+console.log(batter);
+const dictionary = {
+    happy: "feeling good",
+    sad: "feeling not so good",
+    excited: "feeling good about expectations",
+    overexcited: "feeling so good about overexpectation"
+};
+console.log(dictionary);
+console.log(dictionary.excited);
