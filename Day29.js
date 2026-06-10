d9fea38b5b37d921938911b5eff3da2c539f7b7a
amxnandxni <amxnandxni@gmail.com>
2026-06-10 14:15:50 +0530

    Add files via upload
    Day 29 of Learning Web Development:
    Revised JavaScript fundamentals and practiced core concepts

---
+++ Day29.js
@@ -0,0 +1,14 @@
+const name = 'Virat Kohli'
+const city = 'Delhi'
+let age = 37
+// let x = 5;
+// let x =10;
+console.log(null + 1);
+console.log("5" + 3);
+console.log("5" - 3);
+console.log(true + false);
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof 123n);
+console.log(5 == "5");
+console.log(5 === "5"); 
